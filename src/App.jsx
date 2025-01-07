ca202b9054c0f6499afc2ea0c31081bf10844eb7
ApolloData2020 <168612449+ApolloData2020@users.noreply.github.com>
2025-01-06 22:10:36 -0600

    Commit2

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Apollo Data Analytics</h1>
+      <p>Your data-driven sustainability partner.</p>
+    </div>
+  );
+}
+
+export default App;
